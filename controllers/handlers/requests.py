1e3e1c790bc89ebcf077425eea5e77ac75aea98d
Mindy <mtchieu@gmail.com>
2011-12-22 18:46:09 -0500

    Add Vendor Agreement form

--- controllers/handlers/requests.py
+++ controllers/handlers/requests.py
@@ -7,5 +7,33 @@ import logging
 
 class RequestAgreementHandler(BaseHandler):
 	def get(self):
-		self.render('agreement/request.html',
-			title="Request Vendor Agreement")
+
+		empty = {
+			"_xsrf": self.xsrf_token,
+			"id": None,
+			"name": "",
+			"date": "",
+			"amount": "",
+			"summary": "",
+			"client": None,
+			"vendor": None,
+			"actions": [ {
+				"id": "action-request",
+				"capture-id": "request-form",
+				"name": "Request Agreement",
+				"action": "/agreement/request.json",
+				"method": "POST",
+				"params": { }
+			}, {
+					"id": "action-cancel",
+					"capture-id": "request-form",
+					"name": "Cancel Request",
+					"action": "/agreement/request.json",
+					"method": "POST",
+					"params": { }
+				} ],
+			"self": "vendor"
+		}
+
+		title = "New Vendor Agreement &ndash; Wurk Happy"
+		self.render("agreement/request.html", title=title, data=empty)
