b93ba398645e9b64ed108ee7f0ea67e021c98449
Brendan Berg <brendan@plusminusfive.com>
2012-04-20 17:12:41 -0400

    Added some more headers

--- controllers/handlers/tests.py
+++ controllers/handlers/tests.py
@@ -18,6 +18,8 @@ class HTTPServerTestHandler(Authenticated, JSONBaseHandler):
 			('user', self.current_user['id']),
 			('remote_ip', self.request.remote_ip),
 			('protocol', self.request.protocol),
+			('X-Real-IP', self.request.headers['X-Real-IP']),
+			('X-Scheme', self.request.headers['X-Scheme']),
 			('arguments', self.request.arguments)
 		])
 		
